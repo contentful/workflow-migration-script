e1b44fc6db78f16464b7097c80aa6bd052d0ed8b
cniedrich <christian.niedrich@contentful.com>
2022-07-12 10:22:42 +0200

    chore: restructure code

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # workflow-migration-script
+This script provides the functionality to migrate entries assigned to workflows v1 tags to the new workflows v2 feature.
+
+## Installation
+tbd.
+
+## Usage
+tbd.
+
+### Config
+```
+{
+    accessToken: string,
+    spaceId: string,
+    environmentId: string,
+    tags?: string[]
+}
+```
+
+If `tags` are not provided, the script will fetch the current workflow v1 configuration and will start migrating these tags.
--- migrate.js
+++ migrate.js
@@ -1,34 +1,21 @@
 #!/usr/bin/env node
 'use strict';
 
-import fs from 'fs';
 import { program } from 'commander'
-import chalk from 'chalk'
-import boxen from 'boxen'
 import cfManagement from 'contentful-management';
 import inquirer from 'inquirer';
+import { processEntriesInBatch } from './models/migrate-workflow-v1-entries.js'
+import { logBox, warning, error, log, info, action, success, INDENT, bold } from './libs/cli-utils.js';
+import { readJsonFileSync } from './libs/files.js';
 
 const { createClient } = cfManagement;
-const INDENT = ' '
-const DOT = '•'
-const CHECK = '✓'
-const CROSS = '✗'
 
 const SLEEP_MS = 200
 const ALPHA_HEADERS =  {
     ["x-contentful-enable-alpha-feature"]: 'workflow-management-api',
 };
 
-console.log(boxen('Contentful - Workflow Migrator', { padding: 0.5, margin: { top: 0.5, left: 0.5, right: 0.5, bottom: 1 }, dimBorder: true }));
-
-// -----------------------
-const log = (message, indent) => console.log((indent > 0 ? INDENT.repeat(indent) : '') + (message ?? ''))
-const error = (s, i) => log(chalk.bold.red(s), i);
-const success = (s, i) => log(`${chalk.bold.green(CHECK)} ${s}`, i);
-const warning = (w, i) => log(chalk.hex('#FFA500')(w), i); // Orange color
-const info = (m, i) => log(chalk.white(m), i); // Orange color
-const action = (m, i) => info(`${DOT} ${m}`, i); // Orange color
-//------------------------
+logBox('Contentful - Workflow Migrator')
 
 program
     .name('workflow-migrator')
@@ -43,30 +30,21 @@ program.parse();
 const { config: configFilePath, cleanUpTags: shouldCleanUpTags, noDryRun, debounce: debounceMs } = program.opts();
 const dryRun = !(noDryRun ?? false);
 
-//------------------------
-
+//--------------- Script Start ------------------
 if (dryRun) {
     warning('Executing script in dry run mode. Provide a "--noDryRun" flag to perform migration.')
     log()
 }
 
 action('read config file')
+
 // ToDo: resolve relative paths
-if (!fs.existsSync(configFilePath)) {
-    error(`The provided config file does not exist. Path: ${configFilePath}`, 2);
-    process.exit(1)
-}
-
-let config;
-try {
-    const configPlainText = fs.readFileSync(configFilePath);
-    config = JSON.parse(configPlainText);
-} catch (e) {
+let config = readJsonFileSync(configFilePath, (e) => {
     error(`Unable to read json config file. Reason: ${e.message ?? 'Unknown error'}`, 2)
-    process.exit(2)
-}
+    process.exit(1)
+});
 
-const expectedConfig = ['accessToken', 'spaceId', 'environmentId', 'migrationMap']
+const expectedConfig = ['accessToken', 'spaceId', 'environmentId']
 const givenConfig = Object.keys(config)
 const notDefinedConfig = expectedConfig.filter((key) => !givenConfig.includes(key))
 if (notDefinedConfig.length > 0) {
@@ -76,10 +54,10 @@ if (notDefinedConfig.length > 0) {
 success("Config loaded", 2)
 log()
 
-const { spaceId, environmentId , accessToken, migrationMap } = config;
+const { spaceId, environmentId , accessToken, tags: tagsToMigrate } = config;
 
-action('creating contentful client')
-const client = createClient({ accessToken }, {
+action('creating contentful cmaClient')
+const cmaClient = createClient({ accessToken }, {
     type: 'plain',
     alphaFeatures: ['workflows'],
     defaults: { spaceId, environmentId },
@@ -87,27 +65,28 @@ const client = createClient({ accessToken }, {
 
 let environment;
 try {
-    environment = await client.environment.get()
+    environment = await cmaClient.environment.get()
 } catch (e) {
-    error("Error creating client. Please check your config")
+    error("Error creating cmaClient. Please check your config")
+    process.exit(3)
 }
 
 // ToDo: check environment alias
 
-success("Client created", 2)
+success("cma client created", 2)
 log()
 
 info(`start migration`)
 info(`---------------`)
-const oldWorkflowTags = Object.entries(migrationMap)
-if (oldWorkflowTags.length < 1) {
-    error('No workflow map defined, please check your config', 2)
+log()
+if (tagsToMigrate && (tagsToMigrate.length < 1 || tagsToMigrate.filter(t => typeof t !== 'string').length > 0)) {
+    error('Config for tags is invalid. Please provide a tag list as strings.', 2)
     process.exit(3)
 }
 
 let workflowDefinitions;
 try {
-    workflowDefinitions = await client.workflowDefinition.getMany({}, ALPHA_HEADERS)
+    workflowDefinitions = await cmaClient.workflowDefinition.getMany({}, ALPHA_HEADERS)
 } catch (e) {
     error('Error fetching workflow configurations: ' + (e.message ?? 'Unknown Error)', 2));
     process.exit(4)
@@ -131,11 +110,20 @@ const workflowDefinitionIdMap = workflowDefinitions.items.reduce((carry, { sys, 
     return carry;
 }, {})
 
-const tags = await client.tag.getMany({ limit: 500 })
+const tags = await cmaClient.tag.getMany({ limit: 500 })
 if (!tags?.items) {
     error(`No tags found for the conifgured environment with id '${environmentId}'`);
     process.exit(6)
 }
+
+//ToDo: if no tags provided, check app installation
+if (!tagsToMigrate) {
+    // if no tags are provided, fetch app installation of workflow, get config
+    // if config is empty, throw error
+}
+
+const oldWorkflowTags = tagsToMigrate ?? [];
+
 
 // ------------------------- MIGRATION BELOW
 const tagIdToNameMap = tags.items.reduce((carry, tag) => {
@@ -144,103 +132,8 @@ const tagIdToNameMap = tags.items.reduce((carry, tag) => {
 }, {})
 const tagNameToIdMap = Object.fromEntries(Object.entries(tagIdToNameMap).map(a => a.reverse()))
 
-/**
- * Mirgate entries from deprecated workflow to new workflow feature
- * - in batches
- * - including clean up
- */
-async function processEntriesInBatch(tagId, tagName, stepId, workflowDefinition, shouldRemoveTagsFromEntries, totalItems, totalItemsProcessed) {
-    totalItemsProcessed = totalItemsProcessed ?? 0;
-    
-    const entries = await client.entry.getMany({ query: {'metadata.tags.sys.id[in]': tagId }, limit: 100, skip: totalItemsProcessed })
-    if (totalItemsProcessed === 0) {
-        log('')
-        info(`Will start migrating:`, 6)
-        info(`- ${entries.total} entries`, 8)
-        info(`- from tag "${chalk.underline(tagName)}" with id "${tagId}"`, 8)
-        info(`- to "${chalk.underline(workflowDefinition.steps[stepId].name)}" of workflow "${chalk.italic(workflowDefinition.name)}"`, 8)
-
-        const { shouldStartMigration } = await inquirer.prompt({
-            type: 'confirm',
-            name: 'shouldStartMigration',
-            default: false,
-            message: `${INDENT.repeat(6)}Should start migrating entries?`
-        })
-
-        if (!shouldStartMigration) {
-            info(`aborted migration for tag '${tagId}'`, 6)
-            return;
-        }
-
-        shouldRemoveTagsFromEntries = shouldRemoveTagsFromEntries ?? shouldCleanUpTags
-        if (shouldRemoveTagsFromEntries === undefined) {
-            const answerRemoveTagMapping = await inquirer.prompt({
-                type: 'confirm',
-                name: 'shouldRemoveTagsFromEntries',
-                default: false,
-                message: `${INDENT.repeat(6)}Do you want to remove the tag from entries after migration?`
-            })
-            shouldRemoveTagsFromEntries = answerRemoveTagMapping.shouldRemoveTagsFromEntries;
-        }
-
-        totalItems = entries.total
-    }
-
-    if (entries.items.length === 0) {
-        return;
-    }
-
-    for (const entry of entries.items) {
-        await new Promise(r => setTimeout(r, debounceMs)); // mitigate rate limit
-        
-        ++totalItemsProcessed
-        if (!workflowDefinition.enabledContentTypes.includes(entry.sys.contentType.sys.id)) {
-            warning(`${CROSS} ${entry.sys.id} - Entry content type '${entry.sys.contentType.sys.id}' not configured for worklfow '${workflowDefinition.sys.id}'`, 6)
-            continue;
-        }
-        if (!dryRun) {
-            // start new workflow
-            try {
-                await client.workflow.create({}, {
-                    entity: {
-                        type: 'Link',
-                        linkType: 'Entry',
-                        id: entry.sys.id
-                    },
-                    workflowDefinition: {
-                        type: 'Link',
-                        linkType: 'Workflow',
-                        id: workflowDefinition.sys.id
-                    },
-                    stepId
-                }, ALPHA_HEADERS);
-            } catch (e) {
-                error(`${CROSS} ${entry.sys.id} - could not create workflow for entry. Reason ${e.message ?? 'Unknown'}`, 6)
-                continue
-            }
-        }
-        
-        // remove tag
-        if (!dryRun && shouldRemoveTagsFromEntries) {
-            await client.entry.patch({}, [{
-                op: "replace",
-                path: "/metadata/tags",
-                value: entry.metadata.tags.filter(t => t.sys.id !== tagId)
-            }])
-        }
-        
-        success(entry.sys.id, 6)
-    }
-
-    if (totalItems > totalItemsProcessed) {
-       await processEntriesInBatch(tagId, tagName, stepId, workflowDefinition, shouldRemoveTagsFromEntries, totalItems, totalItemsProcessed)
-    }
-}
-
-for (const [oldTag, newWorkflow] of oldWorkflowTags) {
-    action(`process: ${oldTag}`)
+for (const oldTag of oldWorkflowTags) {
     try {
-        info('validate tag', 4)
         let tagId = oldTag
         if (!tagIdToNameMap[tagId]) {
             tagId = tagNameToIdMap[oldTag] ?? null
@@ -248,31 +141,46 @@ for (const [oldTag, newWorkflow] of oldWorkflowTags) {
                 throw new Error(`The tag '${oldTag}' could not be found in the environment`)
             }
         }
+        action(`process "${bold(tagIdToNameMap[tagId])}", id: ${tagId}`)
+        log()
 
-        info('validate workflow', 4)
-        if (!newWorkflow.workflowDefinitionId || !newWorkflow.workflowDefinitionStepId) {
-            throw new Error(`Please provide 'workflowDefinitionId' and 'workflowDefinitionStepId'`)
-        }
-        
-        const workflowDefinition = workflowDefinitionIdMap[newWorkflow.workflowDefinitionId] ?? null
-        if (!workflowDefinition) {
-            throw new Error(`Could not find workflow definition for id '${newWorkflow.workflowDefinitionId}`)
-        }
+        const { workflowDefinitionId } = await inquirer.prompt({
+            type: 'list',
+            name: 'workflowDefinitionId',
+            message: `${INDENT.repeat(2)}Please select the target workflow for migration`,
+            choices: Object.values(workflowDefinitionIdMap).map((w) => ({ name: w.name, value: w.sys.id}))
+        })
 
-        if (!workflowDefinition.stepIds.includes(newWorkflow.workflowDefinitionStepId)) {
-            throw new Error(`Could not find step for workflow step id '${newWorkflow.workflowDefinitionStepId}`)
-        }
-
+        const workflowDefinition = workflowDefinitionIdMap[workflowDefinitionId]
         if (workflowDefinition.sys.isLocked) {
             throw new Error(`Cannot progress with locked workflow, id '${newWorkflow.workflowDefinitionId}`)
         }
+
+        const { workflowDefinitionStepId } = await inquirer.prompt({
+            type: 'list',
+            name: 'workflowDefinitionStepId',
+            message: `${INDENT.repeat(2)}Please select the target workflow step for migration`,
+            choices: Object.values(workflowDefinition.steps).map((s) => ({ name: s.name, value: s.id}))
+        })
         
         info('fetch entries', 4)
-        await processEntriesInBatch(tagId, tagIdToNameMap[tagId], newWorkflow.workflowDefinitionStepId, workflowDefinition)
+        await processEntriesInBatch({
+            tagId,
+            tagName: tagIdToNameMap[tagId],
+            stepId: workflowDefinitionStepId,
+            workflowDefinition,
+            cmaClient,
+            scriptOptions: {
+                dryRun,
+                debounceMs,
+                shouldCleanUpTags
+            }
+        })
 
         // ToDo: shouldCleanUpTags -> remove tag from environment
         success('migration completed', 2)
     } catch (e) {
+        console.log(e)
         error(`Error migrating ${oldTag}: ${e.message ?? 'Unknown Error'}`)
     }
 
